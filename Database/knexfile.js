cded99540e29bd847aa47eae3a87797f0a40c978
Tolterix <tee152222@gmail.com>
2021-01-12 14:22:54 -0600

    container stuff
    Added database and migrations for torkeldb and docker-compose for integrating database with API containers.

---
+++ Database/knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      database: 'torkeldb',
+      user: 'postgres',
+      password: 'testPass',
+      port: '5432'
+    }
+  },
+
+};
---
+++ Database/migrations/20210106114150-setup-user-table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', table => {
+        table.increments('id').notNullable();
+        table.string('first_name').notNullable();
+        table.string('last_name').notNullable();
+        table.string('password').notNullable();
+        table.string('email').notNullable();
+
+        table.unique('email');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('users');
+};
