7aa7bb2311e21fd3ae4113a7f3548e3ec8d04db6
Jonathan Brown <jpbrown87@gmail.com>
2021-01-12 16:32:59 -0600

    Created header components and add logo image

---
+++ torkel/src/components/header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+import Logo from './Torkel_Logo.png'
+
+const Header = () => {
+    return(
+        <div class="header">
+            <img src={Logo} alt='Torkel Logo' height="100px" width="auto"/>
+        </div>
+    )
+
+}
+
+export default Header
+
+
+
+
+
+
+
+
+
