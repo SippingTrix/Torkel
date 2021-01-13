74db1d629bf6ef1eca66f3e3282bac313f79102a
Tolterix <tee152222@gmail.com>
2021-01-13 11:38:54 -0600

    #18 Create Router Functionality on App.js

--- torkel/src/App.js
+++ torkel/src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 import Header from './components/header/Header'
 import Footer from './components/header/Footer' 
 
@@ -7,14 +13,29 @@ import Footer from './components/header/Footer'
 function App() {
 
   return (
-    <>
+    <Router>
       <div className="header">
         <Header />
       </div>
+
+      <Switch>
+        <Route exact path='/'>
+          {/*Put homepage here */}
+        </Route>
+
+        <Route exact path='/cart'>
+          {/*Put cart here */}
+        </Route>
+
+        <Route path='/'>
+          {/*Put homepage here */}
+        </Route>
+      </Switch>
+
       <div className="footer">
-        <Footer />      
+        <Footer />
       </div>
-    </>
+    </Router>
   );
 }
 
