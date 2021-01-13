f8b7295c4c62da258afedce0dd4d6ce74f663dea
Tolterix <tee152222@gmail.com>
2021-01-13 14:40:00 -0600

    Update Login.test.js
    changed wrapper to be in a beforeEach()

--- torkel/src/components/Login/Login.test.js
+++ torkel/src/components/Login/Login.test.js
@@ -4,13 +4,16 @@ import React from "react";
 
 
 describe("Login Screen",  () =>{
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<Login />);
+    })
+        
     it("should render an h1 tag", () =>{
-        const wrapper = shallow(<Login />);
         expect(wrapper.find(".login-header").exists()).toBeTruthy();
     })
 
     it("consist of a username and password field", () =>{
-        const wrapper = shallow(<Login />);
         const requireEmail = wrapper.find("input.email").exists()
         const requirePassword = wrapper.find("input.password").exists()
         expect(requireEmail).toBeTruthy()
@@ -18,20 +21,17 @@ describe("Login Screen",  () =>{
     })
 
     it("should render login button", () => {
-        const wrapper = shallow(<Login />);
         expect(typeof wrapper.find(".login-button").getElement().props.children).toBe("string");
         expect(wrapper.find(".login-button").getElement().props.children).toEqual("Login");
       });
 
     it("should have a register for account option", () => {
-        const wrapper = shallow(<Login />)
         const divRegister = wrapper.find(".register")
         expect(divRegister.exists()).toBeTruthy()
         expect(divRegister.text()).toEqual("Register for an account")
     });
 
     it("should have a forgot password feature", () => {
-        const wrapper = shallow(<Login />)
         const divForgotPassword = wrapper.find(".forgot-password")
         expect(divForgotPassword.exists()).toBeTruthy()
         expect(divForgotPassword.text()).toEqual("Forgot Password")
