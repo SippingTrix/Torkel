50b27b34719d3740511781719a2845a420f97903
SippingTrix <39608585+SippingTrix@users.noreply.github.com>
2021-01-15 13:39:26 -0700

    Update Timer.js
    axios implimented need to finish displaying info on homepage

--- torkel/src/components/HomePage/homePageComponents/TimerList.js
+++ torkel/src/components/HomePage/homePageComponents/TimerList.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import { StateContext } from '../../../State'
 import './TimerList.css'
+import axios from 'axios'
 
 
 export default function TimerList() {
+    let { HomepageListing, apiURL } = React.useContext(StateContext)
+    let context = React.useContext(StateContext)
+
+    React.useEffect(() =>{
+        axios.get(apiURL+`current/runners`)
+            .then(res => {
+                let tArr1 = res.data.map(i => {
+                    return (
+                        `drop off location: ${i.dropOffLocation} drop off time: ${i.dropOffTime}`
+                    )
+                })
+                context.setState({
+                    HomepageListing: {
+                        ...HomepageListing,
+                        Runner: {
+                            ...HomepageListing.Runner,
+                            listings: tArr1
+                        }
+                    }
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps 
 
     return(
         <div className='timers'>
