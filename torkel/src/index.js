aa0976fd82faf08060c6f57195fd0eb200af34fb
brentogunmakin <74428659+brentogunmakin@users.noreply.github.com>
2021-01-14 22:36:41 -0700

    1

--- torkel/src/index.js
+++ torkel/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { StateProvider } from './State';
+import  StateProvider  from './State';
 
 ReactDOM.render(
   <StateProvider>
